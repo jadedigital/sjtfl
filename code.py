01560646cbbe829e797a4f39740294ee5f27c1f8
Aaron <agreene@jadedigital.ca>
2017-05-09 00:52:02 -0230

    bug fix

--- code.py
+++ code.py
@@ -134,7 +134,8 @@ class login:
 	def POST(self):
 		username, password = web.input().username, web.input().password
 		ident = db.select('users', where='username=$username', vars=locals())[0]
-		if bcrypt.checkpw(password, ident['password']):
+		hashed = ident['password']
+		if bcrypt.checkpw(password, hashed.encode('utf8')):
 			print('correct')
 			session.logged = True
 			session.privilege = ident['privilege']
